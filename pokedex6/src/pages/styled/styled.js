6a18d3576e377e6b64932b3fc48182cafef8d6d1
jaqueline lourenco <jaquelineloure50@gmail.com>
2021-07-13 18:02:45 -0300

    Adicionando Card na Pagina Detalhe

--- pokedex6/src/pages/styled/styled.js
+++ pokedex6/src/pages/styled/styled.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+//card home e pokedex
+
 export const Header = styled.div`
   color: #ffff33;
   background-color: #ff6666;
@@ -36,4 +38,85 @@ export const HomeFlexBox = styled.main`
   align-items: center;
   flex-wrap: wrap;
   padding-top: 100px;
+`;
+//page details
+
+export const DetailsFlexBox = styled.main`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding-top: 8%;
+`;
+
+export const ImgFlexBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  align-content: center;
+`;
+
+export const PokeImgContainer = styled.div`
+  background-color: #f2f2f2f2;
+  border-radius: 20px;
+  height: 250px;
+  width: 250px;
+  margin: 20px;
+  box-shadow: 3px 3px 3px 3px #d9d9d9;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+`;
+
+export const TypeMovesFlexBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PokeTypeContainer = styled.div`
+  background-color: #f2f2f2f2;
+  border-radius: 20px;
+  height: 60px;
+  width: 350px;
+  margin: 20px;
+  box-shadow: 3px 3px 3px 3px #d9d9d9;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+export const PokeMovesContainer = styled.div`
+  background-color: #f2f2f2f2;
+  border-radius: 20px;
+  height: 439px;
+  width: 350px;
+  margin: 20px;
+  box-shadow: 3px 3px 3px 3px #d9d9d9;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+`;
+
+export const StatsFlexBox = styled.div`
+  display: flex;
+  justify-content: column;
+  text-align: Center;
+`;
+
+export const PokeStatsContainer = styled.div`
+  background-color: #f2f2f2f2;
+  border-radius: 20px;
+  height: 543px;
+  width: 350px;
+  margin: 20px;
+  box-shadow: 3px 3px 3px 3px #d9d9d9;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
 `;
